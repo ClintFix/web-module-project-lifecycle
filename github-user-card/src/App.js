75c830d3ef59e3bd74878a62b80e38100b68ad0f
ClintFix <clint.fix@gmail.com>
2021-03-09 13:34:59 -0700

    successful axios call that adds info to state based on user input

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,13 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react'
+import axios from 'axios'
+import UserCard from './components/UserCard'
 
 class App extends React.Component {
-  
+  state = {
+    user: '',
+    userData: [],
+    followerData: [],
+  }
+
+  //helper functions:
+  handleChange = (e) => {
+    this.setState({
+      user: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const userUrl = `https://api.github.com/users/${this.state.user}`;
+    const followersUrl = `https://api.github.com/users/${this.state.user}/followers`;
+    
+    const userRequest = axios.get(userUrl);
+    const followerRequest = axios.get(followersUrl);
+
+    axios.all([userRequest, followerRequest])
+      .then(axios.spread((...responses) => {
+        const userResponse = responses[0];
+        const followerResponse = responses[1];
+
+        this.setState({
+          userData: userResponse.data,
+          followerData: followerResponse.data,
+        })
+      }))
+  }
+
   render() {
       return (
         <div className="App">
-            <h1>Hi</h1>
+            <h1>GitHub User Cards</h1>
+            <form onSubmit={this.handleSubmit}>
+              <input value={this.state.user} onChange={this.handleChange} placeholder="Enter GitHub Username"/>
+              <button>🔎 Search</button>
+            </form>
+            <UserCard />
         </div>
     );
   }
---
+++ github-user-card/src/components/UserCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class UserCard extends React.Component {
+
+    render() {
+        return (
+            <div>hi there</div>
+        )
+    }
+}
+
+export default UserCard;
